b87aed42e0ef61c50056c8488132b88a543b2a3c
Văn Tấn Vũ <68262929+tanvu2804@users.noreply.github.com>
2020-07-25 07:22:15 -0700

    Code Shoe Table view
    chua hien duoc view cell

--- ProjectIOS/ShoeTableViewCell.swift
+++ ProjectIOS/ShoeTableViewCell.swift
@@ -2,14 +2,26 @@
 //  ShoeTableViewCell.swift
 //  ProjectIOS
 //
-//  Created by Tran Thang on 7/25/20.
+//  Created by Van Tan Vu on 7/25/20.
 //  Copyright © 2020 OfTung. All rights reserved.
 //
 
 import UIKit
 
 class ShoeTableViewCell: UITableViewCell {
+    private var shoeList = [Shoe]()
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
 
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
     
-
+    @IBOutlet weak var shoeName: UILabel!
+    @IBOutlet weak var shoePrice: UILabel!
+    @IBOutlet weak var shoeIImage: UIImageView!
 }
---
+++ ProjectIOS/ShoeTableViewController.swift
@@ -0,0 +1,100 @@
+//
+//  ShoeTableViewController.swift
+//  ProjectIOS
+//
+//  Created by Van Tan Vu on 7/25/20.
+//  Copyright © 2020 OfTung. All rights reserved.
+//
+
+import UIKit
+
+class ShoeTableViewController: UITableViewController {
+    private var shoeList = [Shoe]()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        loadShoe()
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return shoeList.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "ShoeTableViewCell"
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? ShoeTableViewCell else {
+            fatalError("Can not read the cell")
+        }
+        
+        let shoe = shoeList[indexPath.row]
+        cell.shoeName.text = shoe.name
+        cell.shoePrice.text = shoe.price
+        cell.shoeIImage.image = shoe.image
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    func loadShoe(){
+    if let shoe = Shoe(name: "Biti's hunter X", amount: "20", priceEntered: "200000", price: "500000", image:UIImage(named: "DefaultImage")){
+        shoeList += [shoe]
+        }
+    }
+}
